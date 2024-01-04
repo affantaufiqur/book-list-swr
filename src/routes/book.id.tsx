7a6c2694b6886d11570ddbe7335e4a0ad751f01c
affantaufiqur <affantaufiqur@gmail.com>
2024-01-04 16:44:47 +0700

    better params

--- src/routes/book.id.tsx
+++ src/routes/book.id.tsx
@@ -1,13 +1,11 @@
-import { useRouter } from "@tanstack/react-router";
+import { useParams } from "@tanstack/react-router";
 import { useFetchBookById } from "../hooks/fetch.hook";
 import Spinner from "../components/spinner";
+import Rating from "../components/rating";
 
 export default function BookById() {
-    const router = useRouter();
-    const params = router.state.location.href.split("/").at(-1);
-    const { data, error, isLoading } = useFetchBookById(
-        parseInt(params as string),
-    );
+    const params = useParams({ strict: false });
+    const { data, error, isLoading } = useFetchBookById(parseInt(params.id));
 
     if (error) return <p>Error fetching data</p>;
     if (isLoading) return <Spinner />;
@@ -19,7 +17,10 @@ export default function BookById() {
                     src={data?.image_url}
                     className="h-full w-auto object-contain"
                 />
-                <p className="text-2xl font-semibold">{data?.title}</p>
+                <div className="flex flex-col space-y-4">
+                    <p className="text-2xl font-semibold">{data?.title}</p>
+                    <Rating rating={data?.rating as number} />
+                </div>
             </div>
         </div>
     );
